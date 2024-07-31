62fb60e324e45eab24786a29d53b37425d9f0764
Igor ujiie <igorkazuhico@hotmail.com>
2024-07-31 20:30:33 -0300

    POO

--- src/POO/PrimeiraAula.java
+++ src/POO/PrimeiraAula.java
@@ -3,18 +3,7 @@ package POO;
 public class PrimeiraAula {
     public static void main(String[] args) {
 
-        //sem Poo
-        int idade =30;
-        String nome = "Rodolfho";
-        System.out.println("ola, meu nome é " + nome + " e tenho " + idade + " anos");
-
-        nome = "igor";
-        idade = 25;
-        System.out.println("ola, meu nome é " + nome + " e tenho " + idade + " anos");
-
-        //com Poo
         Pessoa pessoa1 = new Pessoa();
-
         pessoa1.nome = "Kazu";
         pessoa1.idade = 18;
         pessoa1.apresentacao();
